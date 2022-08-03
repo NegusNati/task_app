078a233ba132888d710f957fc240ba1d02715f0c
NATNAEL BIRHANU <negusnati@gmail.com>
2022-08-03 06:26:56 -0700

    security for tasks with UnmodifiableListView

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -2,23 +2,29 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:task_app/models/task.dart';
+import 'dart:collection';
 
 class TaskData extends ChangeNotifier{
 
-List<Task> tasks = [
+List<Task> _tasks = [
     Task(name: 'lol'),
     Task(name: 'nope'),
     Task(name: 'yes'), 
   ];
 
+ UnmodifiableListView<Task> get tasks {
+  return UnmodifiableListView(_tasks);
+ }
+
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
   }
   void addATask(String newTask){
     final task = Task(name: newTask);
-    tasks.add(task);
+    _tasks.add(task);
     notifyListeners();
   }
+
 
 }
 
